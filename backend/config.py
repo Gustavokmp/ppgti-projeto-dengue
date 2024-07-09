85017d727c4543392bb709d15454c18d19afedfa
Gustavo Pires <gustavo.pires@virtus.ufcg.edu.br>
2024-07-09 15:52:17 -0300

    url banco docker

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,6 @@
 import os
 
 class Config:
-    MONGODB_URI = 'mongodb://localhost:27017/dengue_db'
-    # MONGODB_URI = 'mongodb://mongodb:27017/dengue_db'
+    # MONGODB_URI = 'mongodb://localhost:27017/dengue_db'
+    MONGODB_URI = 'mongodb://mongodb:27017/dengue_db'
     API_BASE_URL = 'https://info.dengue.mat.br/api/alertcity'
